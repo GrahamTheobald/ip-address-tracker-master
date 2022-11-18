f6d1d36b5cee367e779b4d46ce098a7299a106c3
Graham Theobald <Graham_Theobald@live.co.uk>
2022-11-18 17:52:32 +0000

    leaflet map api

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mapDiv = document.querySelector("[data-map]")
 const dataDivs = document.querySelectorAll("[data-in]")
 const input = document.querySelector("[data-input]")
 const submit = document.querySelector("[data-submit]")
+const form = document.querySelector("[data-form]")
 
 const API_KEY = 'e08a37d7f9d54287b031e8df856a534a'
 const GEO_URL = 'https://api.ipgeolocation.io/ipgeo?'
@@ -12,14 +13,23 @@ const DIVS = {
   ISP: "isp", 
   TIMEZONE:"timezone"}
 
-submit.addEventListener("click", async () => {
+var map = L.map('map', {zoomControl: false})
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+  render()
+})
+
+async function render() {
   const searchTerm = input.value
+  console.log(input)
   const data = await apiCall(searchTerm)
   if (!data) return
   populate(data)
-})
+  setMap(data.latitude, data.longitude)
+}
 
-async function apiCall(search) {
+async function apiCall(search = '') {
   try {
     const response = await fetch(`${GEO_URL}apiKey=${API_KEY}&ip=${search}`)
     if (response.status === 404 || response.status === 403) return false
@@ -30,6 +40,20 @@ async function apiCall(search) {
     return false
   }
 }
+
+function setMap(latitude, longitude) {
+  map.setView([latitude, longitude], 17);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  var myIcon = L.icon({
+    iconUrl: './images/icon-location.svg',
+  })
+  
+  L.marker([latitude, longitude], {icon: myIcon}).addTo(map);
+  }
 
 function populate(data) {
   const {
@@ -55,3 +79,5 @@ function populate(data) {
       }
     })
 }
+
+render()
